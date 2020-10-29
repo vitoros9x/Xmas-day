f1c2d78f5fb1dbb13139f26c8913f908af4aa231
Tran Tu <68361206+vitoros9x@users.noreply.github.com>
2020-10-29 17:14:40 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-var arrTab = document.querySelectorAll('.shopping__category li');
-
-function appr(a) {
-  var i;
-  for (i = 0; i < arrTab.length; i++) {
-    if (a == i) {
-      arrTab[a].classList.add('active');
-    } else {
-      arrTab[a].classList.remove('active');
-    }
-  }
-}
+$(document).ready(function () {
+  $('.shopping__category li').click(function() {
+    $('.shopping__category li').removeClass('active');
+    $(this).addClass('active');
+  });
+});
