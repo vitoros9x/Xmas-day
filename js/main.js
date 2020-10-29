6598b4fb3c35b3a4940a89c39151009669f0ab03
Tran Tu <68361206+vitoros9x@users.noreply.github.com>
2020-10-29 16:35:27 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-var i;
 var arrTab = document.querySelectorAll('.shopping__category li');
-for (i = 0; i < arrTab.length; i++) {
-  
+
+function appr(a) {
+  var i;
+  for (i = 0; i < arrTab.length; i++) {
+    if (a == i) {
+      arrTab[a].classList.add('active');
+    } else {
+      arrTab[a].classList.remove('active');
+    }
+  }
 }
