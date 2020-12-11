3f48a74c00cf633898efba38281e0ff313922a45
Tran Tu <68361206+vitoros9x@users.noreply.github.com>
2020-12-11 09:59:47 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+// Trang chủ
+
 $('.shopping__category li').click(function() {
   $('.shopping__category li').removeClass('active');
   $(this).addClass('active');
 });
+
+$('.header__menu-cart').click(function () {
+  $('.shopping-cart').fadeToggle();
+});
+
+$('.change-color__primary').click(function() {
+  $('.color__selection').fadeToggle();
+})
+
+$
